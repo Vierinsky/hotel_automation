05306efea0f365ccd5745c1420de677f3aa191e8
Felipe <felipe.viera@mayor.cl>
2026-01-20 11:37:21 -0300

    Writing transform.py. Just set the input csv column configuration and created a dummy csv inside the input\ folder so I can make tests

--- src/transform.py
+++ src/transform.py
@@ -1,3 +1,21 @@
 import pandas as pd
 
 # Actualizar según configuración de planillas a transformar
+
+REQUIRED_COLUMNS  = {
+    "Property",             # Propiedad del hotel (ALMASPDV = Stgo, ALMASPUQ = Pta. Arenas, ALMASPUQX = Pta. Arenas Express)
+    "Confirmation Number",  # String o Int (Ej. 333568932)
+    "Rate",                 # FLOAT
+    # "Balance",
+    "Name",                 # String (Apellido, Nombre)
+    # "Room",
+    "Room Type",
+    "Arrival",              # DATE
+    # "Nights",
+    "Departure",            # DATE
+    "Reservation Type",
+    "Rate Code",            # "Rate Code" son siglas genéricas de las tarifas. Dato importante: Si la sigla contiene "SD" al final, significa "sin desayuno" 
+    "Room Type to Charge",
+    "Travel Agent"          # Esta columna dice a que OTA corresponde la reserva
+}
+
